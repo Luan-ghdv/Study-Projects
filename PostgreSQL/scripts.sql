af9d63f92f98e3b0b0c6fcdd5f54b1d2dac0f7a7
Luan Amaral <75528831+Luan-ghdv@users.noreply.github.com>
2024-06-04 11:19:03 -0300

    04/06/24

---
+++ PostgreSQL/scripts.sql
@@ -0,0 +1,35 @@
+create table cliente (
+	idcliente integer not null, 
+	nome varchar(50) not null, -- varchar é até certo número
+	cpf char(11), -- char é o número exato
+	rg varchar(15),
+	data_nascimento date,
+	genero char(1),
+	profissao varchar(30),
+	nacionalidade varchar(30),
+	logradouro varchar(30),
+	numero varchar(10),
+	complemento varchar(30),
+	bairro varchar(30),
+	municipio varchar(30),
+	uf varchar (30),
+	observacoes text,
+
+	-- primary key
+	constraint pk_cln_idcliente primary key (idcliente)
+)
+
+insert into cliente (idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, uf)
+values (1, 'Manuel', '88828383821', '32323', '2001-01-30', 'M', 'Estudante', 'Brasileira', 'Rua Joaguim Nabuco', '23', 'Casa', 'Cidade Nova', 'Porto Uniao', 'SC')
+
+insert into cliente (idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, uf)
+values (2, 'Geraldo', '12343299291', '56565', '1987-01-04', 'M', 'Engenheiro', 'Brasileira', 'Rua das Limas', '200', 'Ap.', 'Centro', 'Poro Uniao', 'SC')
+
+insert into cliente (idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, uf)
+values (3, 'Carlos', '87732323227', '55463', '1967-09-10', 'M', 'Pedreiro', 'Brasileira', 'Rua das Laranjeiras', '300', 'Apart.', 'Cto.', 'Canoinhas', 'SC')
+
+insert into cliente (idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, uf)
+values (4, 'Adriana', '12321222122', '98777', '1989-09-10', 'F', 'Jornalista', 'Brasileira', 'Rua das Limas', '240', 'Casa', 'São Pedro', 'Porto Vitória', 'PR')
+
+insert into cliente (idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, uf)
+values (5, 'Amanda', '99982838828', '28382', '1991-03-04', 'F', 'Jorn.', 'Italiana', 'Av. Central', '100', null, 'São Pedro', 'General Carneiro', 'PR')
