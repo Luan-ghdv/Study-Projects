f71a60debafd9cd5e9e0b36065c1d57c1ddaef42
Luan Amaral <75528831+Luan-ghdv@users.noreply.github.com>
2024-06-07 11:46:57 -0300

    07/06/24

--- PostgreSQL/scripts.sql
+++ PostgreSQL/scripts.sql
@@ -130,3 +130,40 @@ select nome, data_nascimento from cliente where data_nascimento between '01/01/2
 
 -- 14. O nome do cliente e o logradouro, número, complemento, bairro, município e UF concatenado de todos os clientes
 select nome || ' - ' || logradouro || ' - ' || numero || ' - ' || complemento || ' - ' || bairro || ' - ' || municipio || ' - ' || uf from cliente;
+
+select * from cliente;
+update cliente set nome = 'Teste' where idcliente = 1;
+
+update cliente set nome = 'Adriano', genero = 'M', numero = '241' where idcliente = 4;
+insert into cliente (idcliente, nome) values (16, 'João');
+delete from cliente where idcliente = 16;
+
+-- 1. Insira novos dados na tabela cliente
+insert into cliente (idcliente, nome, cpf, rg, data_nascimento, genero, profissao, municipio, uf) values (16, 'Maicon', '12349596421', '1234', '1965-10-10', 'F', 'Empresário', 'Florianólois', 'PR');
+insert into cliente (idcliente, nome, rg, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, uf) values (17, 'Getúlio', '4631', 'F', 'Estudante', 'Brasileira', 'Rua Central', '343', 'Apartamento', 'Centro', 'Curitiba', 'SC');
+insert into cliente (idcliente, nome, genero, profissao, nacionalidade, numero, complemento) values (18, 'Sandra', 'M', 'Professor', 'Italiana', '12', 'Bloco A');
+
+-- 2. Altere os dados do cliente Maicon
+	-- a. O CPF para 45390569432
+	-- b. O sexo para M
+	-- c. O UF para SC
+update cliente set cpf = '45390569432', genero = 'M', uf = 'SC' where idcliente = 16;
+
+-- 3. Altere os dados do cliente Getúlio
+	-- a. A data de nascimento para 01/04/1978
+	-- b. O sexo para M
+update cliente set data_nascimento = '1978-04-01', genero = 'M' where idcliente = 17;
+
+-- 4. Altere os dados da cliente Sandra
+	-- a. O sexo para F
+	-- b. A profissão para Professora
+	-- c. O número para 123
+update cliente set genero = 'F', profissao = 'Professora', numero = '123' where idcliente = 18;
+
+-- 5. Apague o cliente Maicon
+delete from cliente where idcliente = 16;
+
+-- 6. Apague a cliente Sandra
+delete from cliente where idcliente = 18;
+
+select * from cliente;
