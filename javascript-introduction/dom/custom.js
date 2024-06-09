a61ac9c1822af07d1e6bfc7964a63293278f1d5a
Luan Amaral <75528831+Luan-ghdv@users.noreply.github.com>
2024-06-09 20:51:49 -0300

    09/06/24

---
+++ javascript-introduction/dom/custom.js
@@ -0,0 +1,8 @@
+document.getElementById("titulo");
+titulo.textContent = "Substuindo o Título";
+
+document.getElementById("subtitulo");
+subtitulo.textContent = "Olá, Mundo!";
+
+document.getElementById("lista");
+lista.innerHTML = lista.innerHTML + "<li>item 04</li>";
