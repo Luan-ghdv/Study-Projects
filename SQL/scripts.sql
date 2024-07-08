5be1b165b2c341e6ff7feaffbe281ada5470a8d1
Luan Amaral <75528831+Luan-ghdv@users.noreply.github.com>
2024-07-08 12:03:50 -0300

    08/07/24

--- SQL/scripts.sql
+++ SQL/scripts.sql
@@ -167,3 +167,22 @@ delete from cliente where idcliente = 16;
 delete from cliente where idcliente = 18;
 
 select * from cliente;
+
+create table profissao (
+	idprofissao integer not null,
+  	nome varchar(30) not null,
+  	
+  	constraint pk_prf_idprofissao primary key (idprofissao),
+  	constraint un_prf_nome unique (nome)
+)
+
+insert into profissao (idprofissao, nome) values (1, 'Estudante');
+insert into profissao (idprofissao, nome) values (2, 'Engenheiro');
+insert into profissao (idprofissao, nome) values (2, 'Engenheiro');
+insert into profissao (idprofissao, nome) values (3, 'Pedreiro');
+insert into profissao (idprofissao, nome) values (4, 'Jornalista');
+insert into profissao (idprofissao, nome) values (5, 'Professor');
+
+select * from profissao
+
+select profissao from cliente
